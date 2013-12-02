c3ad3b96b0154ffe11495fc664f006c5f57a794b
Mitch Connors <mconnors@progauge.us>
2013-12-02 14:02:49 -0800

    Resolve more build errors.  This should build completely.

--- examples/Adafruit_L3GD20_test/Adafruit_L3GD20_test.ino
+++ examples/Adafruit_L3GD20_test/Adafruit_L3GD20_test.ino
@@ -16,22 +16,22 @@
  ****************************************************/
 
 #include <Wire.h> 
-#include <Adafruit_L3GD20.h>
+#include "Adafruit_STM_Gyro.h"
 #include <Adafruit_Sensor.h>
 
 // Comment this next line to use SPI
-//#define USE_I2C
+#define USE_I2C
 
 #ifdef USE_I2C
   // The default constructor uses I2C
-  Adafruit_L3GD20 gyro;
+  Adafruit_STM_Gyro gyro;
 #else
   // To use SPI, you have to define the pins
   #define GYRO_CS 4 // labeled CS
   #define GYRO_DO 5 // labeled SA0
   #define GYRO_DI 6  // labeled SDA
   #define GYRO_CLK 7 // labeled SCL
-  Adafruit_L3GD20 gyro(GYRO_CS, GYRO_DO, GYRO_DI, GYRO_CLK);
+  Adafruit_STM_Gyro gyro(GYRO_CS, GYRO_DO, GYRO_DI, GYRO_CLK);
 #endif
 
 void setup() 
@@ -39,20 +39,21 @@ void setup()
   Serial.begin(9600);
   
   // Try to initialise and warn if we couldn't detect the chip
-   if (!gyro.begin(gyro.L3DS20_RANGE_250DPS))
-  //if (!gyro.begin(gyro.L3DS20_RANGE_500DPS))
-  //if (!gyro.begin(gyro.L3DS20_RANGE_2000DPS))
+   if (!gyro.begin(gyro.GYRO_RANGE_250DPS))
+  //if (!gyro.begin(gyro.GYRO_RANGE_500DPS))
+  //if (!gyro.begin(gyro.GYRO_RANGE_2000DPS))
   {
-    Serial.println("Oops ... unable to initialize the L3GD20. Check your wiring!");
+    Serial.println("Oops ... unable to initialize the gyro. Check your wiring!");
     while (1);
   }
 }
 
 void loop() 
 {
-  gyro.read();
-  Serial.print("X: "); Serial.print((int)gyro.data.x);   Serial.print(" ");
-  Serial.print("Y: "); Serial.print((int)gyro.data.y);   Serial.print(" ");
-  Serial.print("Z: "); Serial.println((int)gyro.data.z); Serial.print(" ");
+  sensors_event_t event;
+  gyro.getEvent(&event);
+  Serial.print("X: "); Serial.print((int)event.gyro.x);   Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)event.gyro.y);   Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)event.gyro.z); Serial.print(" ");
   delay(100);
 }
